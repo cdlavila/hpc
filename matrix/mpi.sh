59737dca53fbdb59d104cb59dd49146e538bcc7b
Carlos <carlosdaniel.londono@utp.edu.co>
2023-05-25 22:28:30 -0500

    mpi.sh

--- matrix/mpi.sh
+++ matrix/mpi.sh
@@ -7,7 +7,7 @@ do
   echo "|----|--------------------|--------------------|--------------------|--------------------|" >> mpi_${n}.csv
 
   # Loop over iterations
-  for k in 1 2 # Number of iterations
+  for k in 1 2 3 4 5 6 7 8 9 10 # Number of iterations
   do
     # Print the iteration number
     echo -n "| ${k}  |" >> mpi_${n}.csv
