388719e232bbc95352e569ef2dd8d73d1942788e
Carlos <carlosdaniel.londono@utp.edu.co>
2023-05-25 20:30:05 -0500

    MPI

--- examples/mpi.c
+++ examples/mpi.c
@@ -1,69 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-double execution_time = 0.0;
-
-void multiply_matrices(int **matrix_a, int **matrix_b, int **matrix_result, int size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            int sum = 0;
-            for (int k = 0; k < size; k++) {
-                sum += matrix_a[j][k] * matrix_b[k][i];
-            }
-            matrix_result[j][i] = sum;
-        }
-    }
-}
-
-void read_matrices(int **matrix_a, int **matrix_b, int size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            matrix_a[i][j] = rand() % 10;
-            matrix_b[i][j] = rand() % 10;
-        }
-    }
-}
+#include <mpi.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("You must provide the two arguments: size of the matrix and show matrix (0 or 1)");
-        return 0;
-    }
+    int rank, size;
 
-    srand(time(NULL));
-    int size = atoi(argv[1]);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int **matrix_a = (int **) malloc(size * sizeof(int *));
-    int **matrix_b = (int **) malloc(size * sizeof(int *));
-    int **matrix_result = (int **) malloc(size * sizeof(int *));
+    printf("Hola desde el proceso %d de %d procesos.\n", rank, size);
 
-    for (int i = 0; i < size; i++) {
-        matrix_a[i] = (int *) malloc(size * sizeof(int));
-        matrix_b[i] = (int *) malloc(size * sizeof(int));
-        matrix_result[i] = (int *) malloc(size * sizeof(int));
-    }
-
-    read_matrices(matrix_a, matrix_b, size);
-
-    struct timespec start, end;
-    clock_gettime(CLOCK_REALTIME, &start);
-
-    multiply_matrices(matrix_a, matrix_b, matrix_result, size);
-
-    clock_gettime(CLOCK_REALTIME, &end);
-    execution_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-
-    for (int i = 0; i < size; i++) {
-        free(matrix_a[i]);
-        free(matrix_b[i]);
-        free(matrix_result[i]);
-    }
-    free(matrix_a);
-    free(matrix_b);
-    free(matrix_result);
-
-    printf("Execution time: %f seconds", execution_time);
+    MPI_Finalize();
 
     return 0;
 }
--- matrix/mpi.c
+++ matrix/mpi.c
@@ -1,121 +1,69 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-#include <mpi.h>
-#define num_max 10
 
 double execution_time = 0.0;
-int **matrix_a;
-int **matrix_b;
-int **matrix_result;
-int n;
 
-void multiply_matrices(int start_row, int end_row)
-{
-    for (int i = start_row; i < end_row; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            int add = 0;
-
-            for (int k = 0; k < n; k++)
-            {
-                add += matrix_a[i][k] * matrix_b[k][j];
+void multiply_matrices(int **matrix_a, int **matrix_b, int **matrix_result, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            int sum = 0;
+            for (int k = 0; k < size; k++) {
+                sum += matrix_a[j][k] * matrix_b[k][i];
             }
-            matrix_result[i][j] = add;
+            matrix_result[j][i] = sum;
         }
     }
 }
 
-void read_matrix()
-{
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            matrix_a[i][j] = rand() % num_max;
-            matrix_b[i][j] = rand() % num_max;
+void read_matrices(int **matrix_a, int **matrix_b, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            matrix_a[i][j] = rand() % 10;
+            matrix_b[i][j] = rand() % 10;
         }
     }
 }
 
-int main(int argc, char *argv[])
-{
-    MPI_Init(&argc, &argv);
-
-    int num_processes, process_id;
-    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
-    MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
-
-    if (argc != 2)
-    {
-        if (process_id == 0)
-        {
-            printf("You must provide one argument: size of the matrix\n");
-        }
-        MPI_Finalize();
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("You must provide the two arguments: size of the matrix and show matrix (0 or 1)");
         return 0;
     }
 
     srand(time(NULL));
-    n = atoi(argv[1]);
+    int size = atoi(argv[1]);
 
-    int rows_per_process = n / num_processes;
-    int start_row = process_id * rows_per_process;
-    int end_row = (process_id + 1) * rows_per_process;
+    int **matrix_a = (int **) malloc(size * sizeof(int *));
+    int **matrix_b = (int **) malloc(size * sizeof(int *));
+    int **matrix_result = (int **) malloc(size * sizeof(int *));
 
-    if (process_id == num_processes - 1)
-    {
-        end_row = n;
+    for (int i = 0; i < size; i++) {
+        matrix_a[i] = (int *) malloc(size * sizeof(int));
+        matrix_b[i] = (int *) malloc(size * sizeof(int));
+        matrix_result[i] = (int *) malloc(size * sizeof(int));
     }
 
-    matrix_a = (int **)malloc(n * sizeof(int *));
-    matrix_b = (int **)malloc(n * sizeof(int *));
-    matrix_result = (int **)malloc(n * sizeof(int *));
-
-    for (int i = 0; i < n; i++)
-    {
-        matrix_a[i] = (int *)malloc(n * sizeof(int));
-        matrix_b[i] = (int *)malloc(n * sizeof(int));
-        matrix_result[i] = (int *)malloc(n * sizeof(int));
-    }
-
-    if (process_id == 0)
-    {
-        read_matrix();
-    }
-
-    MPI_Bcast(matrix_a[0], n * n, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(matrix_b[0], n * n, MPI_INT, 0, MPI_COMM_WORLD);
+    read_matrices(matrix_a, matrix_b, size);
 
     struct timespec start, end;
-    MPI_Barrier(MPI_COMM_WORLD); // Synchronize all processes
     clock_gettime(CLOCK_REALTIME, &start);
 
-    multiply_matrices(start_row, end_row);
+    multiply_matrices(matrix_a, matrix_b, matrix_result, size);
 
-    MPI_Barrier(MPI_COMM_WORLD); // Synchronize all processes
     clock_gettime(CLOCK_REALTIME, &end);
-
     execution_time = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
 
-    if (process_id == 0)
-    {
-        printf("%f\n", execution_time);
-    }
-
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < size; i++) {
         free(matrix_a[i]);
         free(matrix_b[i]);
         free(matrix_result[i]);
     }
-
     free(matrix_a);
     free(matrix_b);
     free(matrix_result);
 
-    MPI_Finalize();
+    printf("Execution time: %f seconds", execution_time);
 
     return 0;
 }
